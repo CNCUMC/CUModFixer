50da269de810eaac1d82d3236eeb7174516e81d6
Black_Moss <kytheminecraft@outlook.com>
2026-07-10 13:30:21 +0800

    Initial commit: CUMod Fixer mod

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace CUModFixer;
+
+[BepInPlugin(Guid, Name, Version)]
+public class Plugin : BaseUnityPlugin
+{
+    public const string Guid = "org.cncumc.cumodfixer";
+    public const string Name = "CUMod Fixer";
+    public const string Version = "1.0.0";
+    internal new static ManualLogSource Logger;
+    private readonly Harmony _harmony = new(Guid);
+
+    public void Awake()
+    {
+        Logger = base.Logger;
+        _harmony.PatchAll();
+
+        Logger.LogInfo("CUMod Fixer loaded!");
+    }
+}
